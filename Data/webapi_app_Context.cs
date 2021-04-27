ed42a2850b10e2c09740ac76534338b73ed38082
sahib <sahibjotdeol@gmail.com>
2021-04-28 11:28:25 +1200

    WebApi

---
+++ Data/webapi_app_Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webapi_app_.MyModels;
+
+namespace webapi_app_.Data
+{
+    public class webapi_app_Context : DbContext
+    {
+        public webapi_app_Context (DbContextOptions<webapi_app_Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webapi_app_.MyModels.Item> Item { get; set; }
+    }
+}
---
+++ MyModels/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_app_.MyModels
+{
+    public class Item
+    {
+        //item id
+        public int Id { get; set; }
+
+        //The name of the company
+        public string Company { get; set; }
+
+        //Price of  one item item
+        public decimal Price { get; set; }
+
+        //The total number of items in the company.
+        public int NumberOfitems { get; set; }
+    }
+}
